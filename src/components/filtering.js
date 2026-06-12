e8998e6db3ca0e57a35fa8f2cbe9bd216f575eab
Viktoria Sinyakova <victoriasokolova2000@gmail.com>
2026-06-12 17:45:50 +0300

    First

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,35 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
 // @todo: #4.3 — настроить компаратор
-
+const compare = createComparison(defaultRules);
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
-
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+      .forEach((elementName) => {                        // Перебираем по именам
+        elements[elementName].append(                    // в каждый элемент добавляем опции
+            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+            .map(name => {                        // используйте name как значение и текстовое содержимое
+            // @todo: создать и вернуть тег опции
+            const elem = document.createElement('option');
+            elem.textContent = name;
+            elem.value = name;
+            return elem;
+            })
+        )
+     });
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
-
+        if (action && action.name === 'clear') {
+            const input = document.parentElement.querySelector('input');
+            if (input) {
+                input.value = '';
+            }
+            const field = action.dataset.field;
+            if (field) {
+                state[field] = '';
+            }
+        }
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        return data.filter(row => compare(row, state));
     }
 }
--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,21 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
-
+    const comparator = createComparison({
+        skipEmptyTargetValues: true,
+        rules: [
+        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false)
+        ]
+    });
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        if (!action || action.name !== 'search') {
+        return data; // Если нет действия поиска, возвращаем данные без изменений
+        }
+
+        const searchValue = action.value;
+
+        // Применяем компаратор для фильтрации данных по поисковому запросу
+        return data.filter(item => comparator(item, searchValue));
     }
 }
